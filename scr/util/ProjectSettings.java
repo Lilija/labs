ae0c682cf5e865b6592aaf02d7b2f9079617ceb9
Lilija <m.b.petrovic@gmail.com>
2017-10-20 00:15:40 +0200

    Inicial commit

---
+++ scr/util/ProjectSettings.java
@@ -0,0 +1,8 @@
+package util;
+
+import java.text.DateFormat;
+import java.time.format.DateTimeFormatter;
+
+public class ProjectSettings {
+    public static final DateTimeFormatter LeagueFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+}
